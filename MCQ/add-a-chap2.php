3288a1fff2a2a0be93639ad57022cbcbc3a1fc6a
Asarynal <jarvisnaharoy@gmail.com>
2020-01-09 21:53:21 +0530

    Login Master and Add Chapter ready

--- MCQ/add-a-chap2.php
+++ MCQ/add-a-chap2.php
@@ -4,7 +4,7 @@
 //redirects to the page add-a-chap3.php. please proceed to the
 //else part directly.
 
-
+include 'db_connection.php';
 
 session_start();
 if ( isset($_SESSION['loggedinmaster']) == false ){
@@ -31,14 +31,30 @@ else {
 	$name = $_POST["chapter-name"];
 	//"name" variable here contains the name of the chapter which is to be
 	//added to the database.
-
-	
-	
 	//Please add the chapter to the database here..........
+	$id = preg_replace('/\s/','',$name);
+	$id = strtolower($id);
+	$conn = OpenCon();
+ 	$conn->set_charset("utf8mb4");
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); 
+	try{
+		$stmt = $conn->prepare("INSERT INTO chapters (chapter_id, chapter) VALUES (?, ?)");
+		$stmt->bind_param("ss",$id,$name);
+		$stmt->execute();
+		header('location: add-a-chap3.php');
+	}
+	catch(Exception $e){
+		if($conn->errno === 1062){
+			echo "Duplicate Entry ";
+		}
+		else{
+			echo $e->getMessage();
+		}
+	}
 	
+	$stmt->close();
+ 	CloseCon($conn);
 	
- 
-	header('location: add-a-chap3.php');
 	//After updating the database this file redirects the page 
 	//to "add-a-chap3.php".
 
