94c924239d976e4945fe177c0d5769d04eedabd7
rash42 <40036013+rash42@users.noreply.github.com>
2020-01-27 23:44:22 +0530

    Suppports special symbol

--- MCQ/del-a-ques2.php
+++ MCQ/del-a-ques2.php
@@ -58,7 +58,7 @@ else {
 		return $(this).text();
     }).get();
 	var td=tableData[0];
-	window.location.href = "del-a-ques3.php?ques_id=" + td + "&chapter_del=" + "';echo $chname;echo '";
+	window.location.href = "mod-a-ques3.php?ques_id=" + td + "&chapter_del=" + encodeURIComponent("';echo $chname;echo '") ;
 
 	});
 });	
