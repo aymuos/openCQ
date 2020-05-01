45e98a07fc9f285485b253923ad51edd076877ed
rash42 <40036013+rash42@users.noreply.github.com>
2020-05-01 11:56:25 +0530

    Update receiver_header.php

--- REVAMPED OPENCQ/update1/api/receiver_header.php
+++ REVAMPED OPENCQ/update1/api/receiver_header.php
@@ -6,5 +6,7 @@ define("key","SherlockNeedsWatson");
 define("bank","E:\\explorer\\XAMPP\\xam\\htdocs\\update1\\api\\questionBank\\");
 define("exam","E:\\explorer\\XAMPP\\xam\\htdocs\\update1\\api\\exam\\");
 // define("bank","questionBank");
+$key = key;
+
 
 ?>
