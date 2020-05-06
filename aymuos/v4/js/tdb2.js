b10fb2f9dc439f25b44ec3f4c54c10901fdf3abd
aymuos <sparkacco@gmail.com>
2020-05-07 04:35:28 +0530

    UPDATE 16
    BIG update!
    Added addquestion.html
    Included textbox.io SDK

--- aymuos/v4/js/tdb2.js
+++ aymuos/v4/js/tdb2.js
@@ -26,3 +26,20 @@ document.addEventListener('DOMContentLoaded', function() {
         outDuration:175
     });
   });
+
+
+
+  /*modal*/
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+      opacity:0.5,
+      inDuration:250,
+      outDuration:200,
+      preventScrolling:true,
+      dismissible:true,
+      startingTop:'4%'
+      endingTop:'10%'
+
+    });
+  });
