0ef81551331a7575de06f82154bf4eb212859cdd
Asarynal <jarvisnaharoy@gmail.com>
2020-05-08 11:49:59 +0530

    API updates againgit status!

--- API/api/mod_chap.php
+++ API/api/mod_chap.php
@@ -4,12 +4,12 @@
 	
 	
 	
-	$_POST['key']=key;
-	$_POST['username']="Ben Tennyson";
-	$_POST['password']="AlienX";
-	$_POST['sub_code']="MU250";
-	$_POST['chapter_id']="32";
-	$_POST['new_chapter_name']="abcd     efg    hij    ";
+	// $_POST['key']=key;
+	// $_POST['username']="Ben Tennyson";
+	// $_POST['password']="AlienX";
+	// $_POST['sub_code']="MU250";
+	// $_POST['chapter_id']="32";
+	// $_POST['new_chapter_name']="abcd     efg    hij    ";
 	$valid = checkSet(['key','username','password','sub_code','new_chapter_name','chapter_id'],1);
 	if($valid[0]==0){
 		$arr = array(	'status' => 'FAIL',
