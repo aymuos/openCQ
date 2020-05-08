e916031c406ded3a7bd7bbc7f47a3251bd1aaa29
rash42 <40036013+rash42@users.noreply.github.com>
2020-05-08 11:38:55 +0530

    Addition of question and Test dashboard

--- REVAMPED OPENCQ/update1/teacher-login2.php
+++ REVAMPED OPENCQ/update1/teacher-login2.php
@@ -21,17 +21,6 @@
 		$_SESSION['loggedinteacher'] = true;
 		$_SESSION['usernameteacher'] = $username;
 		$_SESSION['passwordteacher'] = $password;
-<<<<<<< HEAD
-		$data2['key']=key;
-		$data2['username']=$username;
-		$url2 = location."teacher_info.php";
-		$result2 = send_get_request($url2,$data2);
-		$out2 = json_decode($result2);
-		// var_dump($out2);
-
-
-		$_SESSION['teachername']=$out2->name;
-=======
 		
 		$api = "teacher_info.php";
 		$data = array(	"key" => key,
@@ -44,7 +33,6 @@
 		//echo $ans2->name;
 		$_SESSION['name'] = $ans2->name;
 		$_SESSION['code'] = 'CS502';
->>>>>>> 15af29ac9be395a8a33ca08e3554cea5942f6643
 	}
 
 	echo $result;
